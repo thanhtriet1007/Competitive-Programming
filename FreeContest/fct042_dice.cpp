8bd046fcd2eb2fe57a29ef2332f1a864db303052
trie-isme <thanhtriet1007@gmail.com>
2025-03-27 03:04:40 +0700

    add new prob

---
+++ FreeContest/fct042_dice.cpp
@@ -0,0 +1,29 @@
+/* Author : Triet Do Thanh - FPT University */
+
+#include <bits/stdc++.h>
+
+#define endl           '\n'
+#define int            long long
+
+using namespace        std;
+
+typedef pair<int, int> ii;
+
+const int       N      = 1e6 + 7;
+const long long oo     = 1e18 + 7;
+const long long MOD    = 1e9 + 7;
+
+void solve() {
+    // Trie's solution here 
+
+}
+
+#define TASK "test"
+
+signed main()
+{
+   ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+   
+   solve();
+   return 0;
+}
