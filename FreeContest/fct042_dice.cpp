78102a956d93a61b6146219c7b007cea0cb32cd3
trie-isme <thanhtriet1007@gmail.com>
2025-03-27 03:42:12 +0700

    add new

--- FreeContest/fct042_dice.cpp
+++ FreeContest/fct042_dice.cpp
@@ -16,9 +16,17 @@ const long long MOD    = 1e9 + 7;
 int n;
 
 void solve() {
-    // Trie's solution here 
+    // Trie's solution here
     cin >> n;
-    cout << n;
+    int cur = n / 6;
+    int tmp = n % 6;
+    int mi = 0, ma = n * 6;
+    mi += cur;
+    for (int i = 5; i >= 1; --i) {
+        mi += (tmp / i) * (6 - i + 1);
+        if (tmp / i != 0) tmp = tmp % i;
+    }
+    cout << mi << ' ' << ma << endl;
 }
 
 #define TASK "test"
@@ -26,7 +34,10 @@ void solve() {
 signed main()
 {
    ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
-
+   if (fopen("input.txt", "r")) {
+      freopen("input.txt", "r", stdin);
+      freopen("output.txt", "w", stdout);
+   }
    solve();
    return 0;
 }
